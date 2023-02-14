21b12da6890b38c04e98c3fbc8872eeb4345eaf7
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-02-15 00:46:42 +0530

    Added-UC4 checking mobile no.

--- Form.cs
+++ Form.cs
@@ -10,8 +10,11 @@ namespace User_Registration
         public static string Name = "^[A-Z][a-z]{2,}$";
         public static string LastName = "^[A-Z][a-z]{2,}$";
         
-        public static string Email =@"^([a-z.?0-9]{3,}.?[@][a-z0-9]{1,}.(com.au|com.com|in|com|net)| [a-z 0-9]{3,}[@][a-z0-9]{1,}.(com.au|com.com|in|com|net)|[a-z.0-9]{3,}[@][a-z0-9]{1,}.(com.au|com.com|in|com|net)|[a-z0-9]{1,}.?[a-z0-9]{3,}[@][a-z]{3,}.(com|in|net)|[a-z.0-9]{3,}[@][a-z]{3,}.(com.au|com.com))$";
+        public static string Email = @"^([a-z0-9]{1,}.?[a-z0-9]{3,}[@][a-z]{3,}.(com|in|net|com.au|com.com)|
+                                        |[a-z.0-9]{3,}[@][a-z0-9]{1,}.(com.au|com.com|in|com|net)|
+                                        |[a-z.?0-9]{3,}.?[@][a-z0-9]{1,}.(com.au|com.com|in|com|net))$";
 
+        public static string MobileNumber = "^[+]91[ ][6-9]{1}[0-9]{9}$";
 
     }
     public class Form
@@ -33,7 +36,7 @@ namespace User_Registration
                 }
                 else
                 {
-                    Console.WriteLine("\ndone"); 
+                    Console.WriteLine("******done"); 
                 }
 
             }
@@ -52,31 +55,51 @@ namespace User_Registration
                 }
                 else
                 {
-                    Console.WriteLine("\ndone\n"); 
+                    Console.WriteLine("*****done"); 
                 }
 
             }
 
             Regex email = new Regex(Pattern.Email);
-            bool check1 = false;
+             check = false;
             
-            while (!check1)
+            while (!check)
             {
                 Console.Write("Enter your email :");
                 string userEmail = Console.ReadLine();
-                check1 = email.IsMatch(userEmail);
+                check = email.IsMatch(userEmail);
 
-                if (check1 == false)
+                if (check == false)
                 {
                     Console.WriteLine("\nplease follow - please enter right email ");
                 }
                 else
                 {
-                    Console.WriteLine("done");
+                    Console.WriteLine("*****done");
                 }
 
             }
 
+            Regex mobileNumber = new Regex(Pattern.MobileNumber);
+            check = false;
+            Console.WriteLine("\nplease enter country code +91 and a space betweeen code and number");
+
+            while (!check)
+            {
+                Console.Write("\nEnter your Mobile No. : ");
+                string number = Console.ReadLine();
+                check = mobileNumber.IsMatch(number);
+
+                if (check == false)
+                {
+                    Console.WriteLine("\nplease follow - please enter country code +91 and a space betweeen code and number");
+                }
+                else
+                {
+                    Console.WriteLine("******done");
+                }
+
+            }
 
 
         }
